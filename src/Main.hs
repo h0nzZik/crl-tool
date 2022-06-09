fa5807cf9950a7edd62ba63ff26ec72278bdf4fe
Jan Tusil <jenda.tusil@gmail.com>
2022-06-09 19:21:00 +0200

    do something

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,13 @@
-import Kore.Sort
+{-# LANGUAGE TemplateHaskell #-}
+import Kore.Parser
+import System.Environment (getArgs)
+import Data.Text ( Text )
+import Data.Text.IO
 
-main = putStrLn "Hello, World!"
+main :: IO ()
+main = do
+    Prelude.putStrLn "Hello, World!" ;
+    (fileName:_) <- getArgs
+    contents <- Data.Text.IO.readFile fileName
+    print (parseKoreDefinition fileName contents)
+
