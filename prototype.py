8ab66d1e873aa1d2ddf829bccb2e50e42d58ded8
Jan Tusil <jenda.tusil@gmail.com>
2023-01-12 14:11:54 +0100

    Create prototype.py

---
+++ prototype.py
@@ -0,0 +1,44 @@
+# Checks an implication between two ECLPs.
+# Returns a substitution or a None
+def eclp_check_impl(Phi, Psi):
+    pass
+
+# user_cutpoints - List of "lockstep invariants" / "circularities" provided by user;
+#   each one is a CLP. Note that they have not been proved to be valid;
+#   it is our task to verify them if we need to use them.
+# instantiated_cutpoints
+# flushed_cutpoints
+def verify(S, Phi, Psi, user_cutpoints, instantiated_cutpoints = [], flushed_cutpoints = []):
+    if (subst := eclp_check_impl(Phi, Psi)) is not None:
+        return True # build a proof object using subst, Conseq, Reflexivity
+    
+    # For each user cutpoint we compute a substitution which specialize it to the current 'state', if possible.
+    flushed_cutpoints_with_subst = [(PhiC, eclp_check_impl(Phi, PhiC)) for PhiC in flushed_cutpoints]
+    # Is there some flushed cutpoint / axiom which matches our current state? If so, we are done.
+    usable_flushed_cutpoints = [(PhiC, subst) for (PhiC, subst) in flushed_cutpoints_with_subst if subst is not None]
+    if (len(list(usable_flushed_cutpoints)) > 0):
+        return True # Conseq, Axiom
+
+    # For each user cutpoint we compute a substitution which specialize it to the current 'state', if possible.
+    user_cutpoints_with_subst = [(PhiC, eclp_check_impl(Phi, PhiC)) for PhiC in user_cutpoints]
+    # The list of cutpoints matching the current 'state'
+    usable_cutpoints = [(PhiC, subst) for (PhiC, subst) in user_cutpoints_with_subst if subst is not None]
+    # If at least on succeeds, it is good.
+    for (PhiC, subst) in usable_cutpoints:
+        # apply Conseq (using [subst]) to change the goal to [PhiC]
+        # apply Circularity
+        # We filter [user_cutpoints] to prevent infinite loops
+        if verify(S, PhiC, Psi,
+            user_cutpoints=[x for x in user_cutpoints if check_impl(x, PhiC) is None],
+            instantiated_cutpoints=(instantiated_cutpoints + [PhiC]),
+            flushed_cutpoints=flushed_cutpoints
+        ):
+            return True
+    
+    for j in range(0, arity_of(Phi)):
+        step_result = make_step(Phi.component[j])
+        for (b in branches_of(step_result)):
+            if verify(S, Phi.with_component(j, b), user_cutpoints=user_cutpoints, instantiated_cutpoints=[], flushed_cutpoints=instantiated_cutpoints+flushed_cutpoints):
+                return True
+    
+    return False
