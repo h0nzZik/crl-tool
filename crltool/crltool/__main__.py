250d27f454d2ed6fe54abd4cec16213ae3918091
Jan Tusil <jenda.tusil@gmail.com>
2023-01-19 13:41:14 +0100

    restructure

--- crltool/crltool/__main__.py
+++ crltool/crltool/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import logging
 import time
+import sys
 
 from pathlib import Path
 
@@ -65,6 +66,7 @@ def create_argument_parser() -> argparse.ArgumentParser:
     subparser_check_implication = subparsers.add_parser('check-implication', help='Checks whether the specification holds trivially')
     subparser_check_implication.add_argument('--specification', required=True)
     subparser_check_implication.add_argument('--through-list', dest='through_list', action='store_true', default=False)
+    subparser_check_implication.add_argument('--store-witness-to')
 
     subparser_prove = subparsers.add_parser('prove', help='Prove a specification')
     subparser_prove.add_argument('--specification', required=True)
@@ -79,6 +81,58 @@ def create_argument_parser() -> argparse.ArgumentParser:
     
     return argument_parser
 
+def check_implication(rs: ReachabilitySystem, args) -> int:
+    with open(args['specification'], 'r') as spec_f:
+        claim = Claim.from_dict(json.loads(spec_f.read()))
+        if args['through_list']:
+            result : EclpImpliesResult = eclp_impl_valid_trough_lists(rs, claim.antecedent, claim.consequent)
+        else:
+            result = eclp_impl_valid(rs, claim.antecedent, claim.consequent)    
+        print(result.valid)
+        if result.witness is not None:
+            pretty_witness = rs.kprint.kore_to_pretty(result.witness)
+            with open(args['store_witness_to'], 'w') as fw:
+                fw.write(pretty_witness)
+        return 0
+    return 1
+
+def check_implication_directly(rs: ReachabilitySystem, args) -> int:
+    with open(args['pattern'], 'r') as fr:
+        pat = KoreParser(fr.read()).pattern()            
+        print('Input')
+        print(rs.kprint.kore_to_pretty(pat))
+        match pat:
+            case Implies(_, l, r):
+                lhs, rhs = l, r
+            case _:
+                raise ValueError(f"Expected implication, but {pat} was given")
+        impl_result = rs.kcs.client.implies(lhs, rhs)
+        print('Simplified')
+        print(rs.kprint.kore_to_pretty(impl_result.implication))
+        #print(impl_result.implication.text)
+        if (impl_result.satisfiable):
+            print("Satisfiable")
+        else:
+            print("Unsatisfiable")
+        if impl_result.substitution is not None:
+            print("Substitution:")
+            print(rs.kprint.kore_to_pretty(impl_result.substitution))
+            #print(impl_result.substitution.text)
+        if impl_result.predicate is not None:
+            print("Predicate:")
+            print(rs.kprint.kore_to_pretty(impl_result.predicate))
+            #print(impl_result.predicate.text)
+    return 0
+
+def simplify(rs: ReachabilitySystem, args) -> int:
+    with open(args['pattern'], 'r') as fr:
+        pat = KoreParser(fr.read()).pattern()
+        patsimpl0 : Pattern = rs.kcs.client.simplify(pat)
+        print(patsimpl0.text)
+        with open(args['output_file'], 'w') as fw:
+            fw.write(patsimpl0.text)
+    return 0
+    
 def main() -> None:
     argument_parser = create_argument_parser()
     args = vars(argument_parser.parse_args())
@@ -89,60 +143,22 @@ def main() -> None:
     kore_rpc_args : Tuple[str,...] = ()
     if args['kore_rpc_args'] is not None:
         kore_rpc_args = tuple(str.split(args['kore_rpc_args']))
-    print(args)
+    #print(args)
     with ReachabilitySystem(
         definition_dir=Path(args['definition']), 
         kore_rpc_args=kore_rpc_args, 
         connect_to_port=args['connect_to_port'],
         ) as rs:
         if args['command'] == 'check-implication':
-            with open(args['specification'], 'r') as spec_f:
-                claim = Claim.from_dict(json.loads(spec_f.read()))
-            if args['through_list']:
-                result : EclpImpliesResult = eclp_impl_valid_trough_lists(rs, claim.antecedent, claim.consequent)
-            else:
-                result = eclp_impl_valid(rs, claim.antecedent, claim.consequent)    
-            print(f"Implication result: {result}")
-            if result.witness is not None:
-                print(rs.kprint.kore_to_pretty(result.witness))
-
+            retval = check_implication(rs, args)
         elif args['command'] == 'prove':
             print("Dummy proving...")
         elif args['command'] == 'check-implication-directly':
-            with open(args['pattern'], 'r') as fr:
-                pat = KoreParser(fr.read()).pattern()
-            
-            print('Input')
-            print(rs.kprint.kore_to_pretty(pat))
-            match pat:
-                case Implies(_, l, r):
-                    lhs, rhs = l, r
-                case _:
-                    raise ValueError(f"Expected implication, but {pat} was given")
-            impl_result = rs.kcs.client.implies(lhs, rhs)
-            print('Simplified')
-            print(rs.kprint.kore_to_pretty(impl_result.implication))
-            #print(impl_result.implication.text)
-            if (impl_result.satisfiable):
-                print("Satisfiable")
-            else:
-                print("Unsatisfiable")
-            if impl_result.substitution is not None:
-                print("Substitution:")
-                print(rs.kprint.kore_to_pretty(impl_result.substitution))
-                #print(impl_result.substitution.text)
-            if impl_result.predicate is not None:
-                print("Predicate:")
-                print(rs.kprint.kore_to_pretty(impl_result.predicate))
-                #print(impl_result.predicate.text)
-            
+            retval = check_implication_directly(rs, args)
         elif args['command'] == 'simplify':
-            with open(args['pattern'], 'r') as fr:
-                pat = KoreParser(fr.read()).pattern()
-            patsimpl0 : Pattern = rs.kcs.client.simplify(pat)
-            print(patsimpl0.text)
-            with open(args['output_file'], 'w') as fw:
-                fw.write(patsimpl0.text)
+            retval = simplify(rs, args)
+        else:
+            retval = 1
                 
-
+    sys.exit(retval)
         
